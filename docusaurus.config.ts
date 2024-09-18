eb5444126ca351410b67264081b7a5dfb25b7dcb
junho <yunjunhojj@gmail.com>
2024-09-19 01:48:44 +0900

    chore: Update docusaurus.config.ts to use @aldridged/docusaurus-plugin-lunr for search functionality

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  // plugins: [require.resolve("docusaurus-lunr-search")],
+  plugins: ["@aldridged/docusaurus-plugin-lunr"],
 
   title: "antennasm blog",
   tagline: "think, write, share",
